8af98dc5203daedfcc4ce0dc5d8ad8d2667ea8be
Pablo Facciano <pablo.e.facciano@gmail.como>
2026-07-17 18:52:14 -0300

    improve 404 page ui

--- frontend/src/pages/AppError404.vue
+++ frontend/src/pages/AppError404.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="p-8">
-    <h1 class="text-2xl mb-4">Error 404</h1>
-    <p>Not found</p>
+  <div class="p-8 space-y-4">
+    <h1 class="text-2xl">Error 404</h1>
+    <p>Página no encontrada</p>
+    <p><button @click="goBack" class="text-app underline cursor-pointer">Volver atrás</button></p>
   </div>
 </template>
 
@@ -14,6 +15,15 @@ export default defineComponent({
   },
   components: {},
   computed: {},
-  methods: {},
+  methods: {
+    goBack(){
+      // If there is no previous internal history state, redirect to a fallback home route
+      if (window.history.state && window.history.state.back === null) {
+        this.$router.push("/");
+      } else {
+        this.$router.back();
+      }
+    }
+  },
 })
 </script>
