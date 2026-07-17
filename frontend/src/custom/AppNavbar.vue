74791d425f5401b7ea618dd64b22427e26c3119c
Pablo Facciano <pablo.e.facciano@gmail.como>
2026-07-17 19:01:16 -0300

    main sidebar slide animation

--- frontend/src/custom/AppNavbar.vue
+++ frontend/src/custom/AppNavbar.vue
@@ -2,28 +2,30 @@
   <!-- AppNavbar -->
   <div class="bg-sky-600 text-white text-xl text-start flex items-center space-x-4">
     <!-- Sidebar -->
-    <div
-      v-if="showSidebar"
-      @click="showSidebar = !showSidebar"
-      class="flex flex-col bg-sky-600 shadow-sm fixed top-0 bottom-0 left-0 w-9/10"
-    >
-      <div class="p-4 border-b border-app-darker flex space-x-3 items-center">
-        <img
-          class="w-6 h-6"
-          src="https://img.icons8.com/ffffff/ios-filled/50/back--v1.png"
-          alt="menu--v1"
-        />
-        <div class="font-medium">GymApp</div>
-      </div>
+    <Transition name="slide-left">
+      <div
+        v-if="showSidebar"
+        @click="showSidebar = !showSidebar"
+        class="flex flex-col bg-sky-600 shadow-sm fixed top-0 bottom-0 left-0 w-9/10 sm:w-auto sm:min-w-60 cursor-pointer"
+      >
+        <div class="p-4 border-b border-app-darker flex space-x-3 items-center">
+          <img
+            class="w-6 h-6"
+            src="https://img.icons8.com/ffffff/ios-filled/50/back--v1.png"
+            alt="menu--v1"
+          />
+          <div class="font-medium">GymApp</div>
+        </div>
 
-      <RouterLink
-        class="p-4 border-b border-app-darker"
-        v-for="link in links"
-        :key="link.to"
-        :to="link.to"
-        v-text="link.label"
-      ></RouterLink>
-    </div>
+        <RouterLink
+          class="p-4 border-b border-app-darker"
+          v-for="link in links"
+          :key="link.to"
+          :to="link.to"
+          v-text="link.label"
+        ></RouterLink>
+      </div>
+    </Transition>
     <!-- Navbar -->
     <button @click="showSidebar = !showSidebar" class="p-4 cursor-pointer">
       <img
@@ -72,3 +74,28 @@ export default defineComponent({
   },
 })
 </script>
+
+<style lang="css" scoped>
+
+/* 1. Define active states for duration and timing function */
+.slide-left-enter-active,
+.slide-left-leave-active {
+  transition: transform 0.3s ease, opacity 0.3s ease;
+}
+
+/* 2. Define starting state for entering & ending state for leaving */
+.slide-left-enter-from,
+.slide-left-leave-to {
+  transform: translateX(-100%);
+  opacity: 0; /* Optional: adds a fade effect along with the slide */
+}
+
+/* 3. Define ending state for entering & starting state for leaving (Optional) */
+.slide-left-enter-to,
+.slide-left-leave-from {
+  transform: translateX(0);
+  opacity: 1;
+}
+
+
+</style>
