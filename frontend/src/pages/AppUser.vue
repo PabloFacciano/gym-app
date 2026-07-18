49cc4a68eb873e8eb88f2d3731ad1d4334453b31
Pablo Facciano <pablo.e.facciano@gmail.como>
2026-07-17 21:48:14 -0300

    app user page

--- frontend/src/pages/AppUser.vue
+++ frontend/src/pages/AppUser.vue
@@ -2,39 +2,36 @@
   <!-- AppUser -->
   <div>
     <AppNavbar />
-    <div>
-      <table>
-        <tr>
-          <td>id</td>
-          <td>{{ user?.id }}</td>
-        </tr>
-        <tr>
-          <td>name</td>
-          <td>{{ user?.name }}</td>
-        </tr>
-        <tr>
-          <td>email</td>
-          <td>{{ user?.email }}</td>
-        </tr>
-        <tr>
-          <td>picture</td>
-          <td>
-            <img
-              class="h-10 w-10 rounded-full"
-              :src="user?.picture ?? 'https://placehold.co/40x40?text=??'"
-              :alt="user?.name ?? 'user avatar'"
-            />
-          </td>
-        </tr>
-      </table>
+    <!-- Profile -->
+    <div class="bg-neutral-600 p-8 flex flex-col space-y-6 sm:space-y-0 sm:flex-row sm:space-x-6 items-center">
+      <div class="flex justify-center items-center text-center sm:text-start sm:order-1">
+        <img
+          class="w-20 h-auto rounded-full"
+          :src="user?.picture ?? 'https://placehold.co/40x40?text=??'"
+          :alt="user?.name ?? 'user avatar'"
+        />
+      </div>
+
+      <div class="flex flex-col space-y-2 text-center sm:text-start sm:order-1">
+        <div class="text-2xl font-medium">{{ user?.name }}</div>
+        <div>{{ user?.email }}</div>
+      </div>
     </div>
+    <!-- Actions -->
+    <div class="py-8 px-4 sm:px-8">
+      <div class="flex flex-col sm:flex-row">
+        <AppButton size="md" type="secondary" @click="signOut_click">Cerrar Sesión</AppButton>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script lang="ts">
+import AppButton from '@/components/AppButton.vue';
 import AppNavbar from '@/custom/AppNavbar.vue'
 import { AuthStore } from '@/stores/auth'
-import { mapState } from 'pinia'
+import { mapActions, mapState } from 'pinia'
 import { defineComponent } from 'vue'
 
 interface State {}
@@ -47,12 +44,22 @@ export default defineComponent({
   },
   components: {
     AppNavbar,
+    AppButton
   },
   watch: {},
   computed: {
     ...mapState(AuthStore, ['user']),
   },
-  methods: {},
+  methods: {
+    ...mapActions(AuthStore, [ 'signOut' ]),
+    async signOut_click(){
+      try {
+        await this.signOut()
+      } catch (error) {
+        window.alert('SignOut Error')
+      }
+    }
+  },
   mounted() {
     //
   },
